81b2ec41512dc4bfb16be84f07684fe408743c85
Anmol Soni <103637964+Anmolsoni04@users.noreply.github.com>
2024-01-11 16:57:53 +0530

    FinalProject

---
+++ chatapp/static/js/scripts.js
@@ -0,0 +1,15 @@
+console.log("You have to work")
+
+window.addEventListener('DOMContentLoaded', event => {
+    const sidebarToggle = document.body.querySelector('#sidebarToggle');
+    if(sidebarToggle){
+        if(localStorage.getItem('sb|sidebar-toggle') === 'true'){
+            document.body.classList.toggle('sb-sidenav-toggled');
+        }
+        sidebarToggle.addEventListener('click', event => {
+            event.preventDefault();
+            document.body.classList.toggle('sb-sidenav-toggled');
+            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+        });
+    }
+});
